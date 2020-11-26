b2cda9dda0550f6aa3d6d70509e441e8fb6c40b4
Adrien Gallouët <adrien@gallouet.fr>
2020-11-26 18:54:19 +0000

    Better 0.35 strat
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- hatgame.c
+++ hatgame.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <limits.h>
 
+#define ULL_BIT (sizeof(unsigned long long) * CHAR_BIT)
+
 #ifdef __linux__
 #include <sys/random.h>
 #endif
@@ -20,19 +22,40 @@ rnd(void *buf, size_t size)
 static int
 isset(unsigned long long x, int n)
 {
-    return 1 & (x >> (sizeof(x) * CHAR_BIT - (n + 1)));
+    return 1 & (x >> (ULL_BIT - (n + 1)));
+}
+
+static int
+strat(unsigned long long x)
+{
+#if STRAT == 1
+    return 0;
+#elif STRAT == 2
+    return __builtin_clzll(x);
+#else
+    for (int i = 0; i < ULL_BIT; i += 3) {
+        switch (7 & (x >> i)) {
+            case 0: case 7: continue;
+            case 1: case 3: return ULL_BIT - (3 + i);
+            case 2: case 6: return ULL_BIT - (2 + i);
+            case 4: case 5: return ULL_BIT - (1 + i);
+        }
+    }
+    return 0;
+#endif
 }
 
 int
 main(int argc, char **argv)
 {
-    size_t ok = 0, count = 0;
+    size_t win = 0, tot = 0;
+
     while (1) {
         unsigned long long x[2];
         rnd(x, sizeof(x));
-        ok += isset(x[0], __builtin_clzll(x[1])) && isset(x[1], __builtin_clzll(x[0]));
-        count++;
-        printf("  %6zu %6zu  => %3.3f\r", ok, count, (double)ok/(double)count);
+        win += isset(x[0], strat(x[1])) && isset(x[1], strat(x[0]));
+        tot++;
+        printf("  win %-10zu  total %-10zu  probability %.2f\r", win, tot, (double)win/(double)tot);
     }
     return 0;
 }
