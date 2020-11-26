02610cf4eb2c9fd6b6de7cc9acd52aa309449372
Adrien Gallouët <adrien@gallouet.fr>
2020-11-26 12:34:28 +0000

    Support Linux and BSD
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- hatgame.c
+++ hatgame.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+
+#ifdef __linux__
 #include <sys/random.h>
+#endif
+
+void
+rnd(void *buf, size_t size)
+{
+#ifdef __linux__
+    if (getrandom(buf, size, 0) != (ssize_t)size)
+        exit(1);
+#else
+    arc4random_buf(buf, size);
+#endif
+}
 
 static int
 isset(unsigned long long x, int n)
@@ -14,8 +29,7 @@ main(int argc, char **argv)
     size_t ok = 0, count = 0;
     while (1) {
         unsigned long long x[2];
-        if (getrandom(x, sizeof(x), 0) != (ssize_t)sizeof(x))
-            return -1;
+        rnd(x, sizeof(x));
         ok += isset(x[0], __builtin_clzll(x[1])) && isset(x[1], __builtin_clzll(x[0]));
         count++;
         printf("  %6zu %6zu  => %3.3f\r", ok, count, (double)ok/(double)count);
